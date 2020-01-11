c19e81e8fd623b6c1107b5497f8fccd2e34f36b1
mharb <mohamed.harb@cegedim.com>
2020-01-11 10:15:37 +0200

    Put p002 in standard format

--- python-project-euler-100/p002.py
+++ python-project-euler-100/p002.py
@@ -1,24 +1,16 @@
-max = 4000000
-previous = 1
-current = 2
-sequence = []
+def run():
+	max = 4000000
+	previous = 1
+	current = 2
+	sum = current
 
-sequence.append(previous)
-sequence.append(current)
+	while current < max:
+		next = previous + current
+		if (next < max) and (next % 2 == 0):
+			sum += next
+		previous = current
+		current = next
 
-while current < max:
-	next = previous + current
-	if next < max:
-		sequence.append(next)
-	previous = current
-	current = next
+	print('p002:', sum)
 
-print(sequence)
-print('Length of sequence:', len(sequence))
-
-sum = 0
-for number in sequence:
-	if number % 2 == 0:
-		sum = sum + number
-
-print(sum)
+run()
