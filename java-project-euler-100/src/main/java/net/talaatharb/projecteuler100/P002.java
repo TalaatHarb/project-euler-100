7b438752add87a87d8fb5833ac863c098f4c53c5
ABD EL RAOUF Ahmed <ahmed.abd-el-raouf@cegedim.com>
2020-01-13 15:11:44 +0200

    java-p002 feature resolved

---
+++ java-project-euler-100/src/main/java/net/talaatharb/projecteuler100/P002.java
@@ -0,0 +1,39 @@
+package net.talaatharb.projecteuler100;
+
+/**
+ * @author mharb
+ *
+ */
+public class P002 implements Runnable {
+
+	/**
+	 * Problem solution entry point
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new P002().run();
+	}
+
+	private static final double NANO_TO_S = 100000000.0;
+
+	@Override
+	public void run() {
+		final long startTime = System.nanoTime();
+		long max = 4000000;
+		int previous = 1;
+		int current = 2;
+		int sum = current;
+		while (current < max) {
+			int next = previous + current;
+			if ((next < max) && (next % 2 == 0))
+				sum += next;
+			previous = current;
+			current = next;
+		}
+
+		final double period = (System.nanoTime() - startTime) / NANO_TO_S;
+		System.out.println("p002: " + sum + " -> " + period + " s");
+	}
+
+}
