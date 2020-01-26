dfef5d9d03228c9b30189838537605d393de4143
mharb <mohamed.harb@cegedim.com>
2020-01-26 12:25:14 +0200

    Fixed solution for p013 in java

--- java-project-euler-100/src/main/java/net/talaatharb/projecteuler100/P013.java
+++ java-project-euler-100/src/main/java/net/talaatharb/projecteuler100/P013.java
@@ -3,6 +3,7 @@ package net.talaatharb.projecteuler100;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,15 +49,13 @@ public class P013 implements Runnable {
 	@Override
 	public void run() {
 		long startTime = System.nanoTime();
-		long sum = 0;
+		BigInteger sum = new BigInteger("0");
 		final List<String> numbers = readNumbers("p013.txt");
-		final int n = 15;
 
 		for (String line : numbers) {
-			sum += Long.parseLong(line.substring(line.length() - n));
+			sum = sum.add(new BigInteger(line));
 		}
-
-		final long result = sum % ((long) Math.pow(10, 10));
+		final long result = Long.parseLong(sum.toString().substring(0, 10));
 		double totalTime = (System.nanoTime() - startTime) / NANO_TO_S;
 		System.out.println("p013: " + result + " -> " + totalTime + " s");
 
