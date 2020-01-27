9309875c978167aa864c49df2526872ba0e46a75
mharb <mohamed.harb@cegedim.com>
2020-01-27 11:05:40 +0200

    Fixed a typo for 19 in p017 in python

--- python-project-euler-100/p017.py
+++ python-project-euler-100/p017.py
@@ -32,15 +32,11 @@ def run():
     n = 1000
     result = 0
     mem_cache = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
-                 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: '	nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'}
+                 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'}
 
     for i in range(1, n + 1):
         data, mem_cache = number_as_letters(i, mem_cache)
         result += length_of_string(data)
-
-    print('Key,Value,length')
-    for key, value in mem_cache.items():
-        print('{},{},{}'.format(key, value, length_of_string(value)))
 
     total_time = time.time() - start_time
     print('p017:', result, '->', total_time, 's')
