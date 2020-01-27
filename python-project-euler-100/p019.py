dae9161ba72491ffa5117c0a82ea22df15e349f6
mharb <mohamed.harb@cegedim.com>
2020-01-27 17:47:46 +0200

    Added code to handle date and time calculations for p019 in python

--- python-project-euler-100/p019.py
+++ python-project-euler-100/p019.py
@@ -1,9 +1,21 @@
 import time
+import datetime
 
 
 def run():
     start_time = time.time()
     result = 0
+    first_day = datetime.date(1900,1,1)
+    delta = datetime.timedelta(days=6)
+    first_sunday = first_day + delta
+    delta = datetime.timedelta(days=7)
+    current = first_sunday
+    last_day = datetime.date(2000,1,1)
+    while (last_day - current).days > 0:
+        current = current + delta
+        if (current.day == 1):
+            print(current)
+            result += 1
     
     total_time = time.time() - start_time
     print('p019:', result, '->', total_time, 's')
