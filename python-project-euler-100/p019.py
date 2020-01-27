2e68c4d9c1fd80dc3ccff1bf5fa8443c68f29be3
mharb <mohamed.harb@cegedim.com>
2020-01-27 14:44:52 +0200

    Added files for p019 in python

---
+++ python-project-euler-100/p019.py
@@ -0,0 +1,11 @@
+import time
+
+
+def run():
+    start_time = time.time()
+    result = 0
+    
+    total_time = time.time() - start_time
+    print('p019:', result, '->', total_time, 's')
+
+run()
