728c806dbf2f27393f2c467904c3f6bbee33a404
mharb <mohamed.harb@cegedim.com>
2020-02-01 13:01:45 +0200

    Fixed solution for p019 in python, matching the problem

--- python-project-euler-100/p019.py
+++ python-project-euler-100/p019.py
@@ -5,18 +5,16 @@ import datetime
 def run():
     start_time = time.time()
     result = 0
-    first_day = datetime.date(1900,1,1)
-    delta = datetime.timedelta(days=6)
+    first_day = datetime.date(1901,1,1)
+    delta = datetime.timedelta(days=5)
     first_sunday = first_day + delta
     delta = datetime.timedelta(days=7)
     current = first_sunday
-    last_day = datetime.date(2000,1,1)
+    last_day = datetime.date(2000,12,31)
     while (last_day - current).days > 0:
         current = current + delta
         if (current.day == 1):
-            print(current)
             result += 1
-    
     total_time = time.time() - start_time
     print('p019:', result, '->', total_time, 's')
 
