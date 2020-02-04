2dfba11659a3baffd0fd58bbede86b09bcadad89
mharb <mohamed.harb@cegedim.com>
2020-02-04 11:33:55 +0200

    Added solution for p019 in java

--- java-project-euler-100/src/main/java/net/talaatharb/projecteuler100/P019.java
+++ java-project-euler-100/src/main/java/net/talaatharb/projecteuler100/P019.java
@@ -1,5 +1,8 @@
 package net.talaatharb.projecteuler100;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  * P019
  * 
@@ -22,8 +25,25 @@ public class P019 implements Runnable {
 	@Override
 	public void run() {
 		final long startTime = System.nanoTime();
+		
 		int result = 0;
 		
+		final GregorianCalendar gcalendar = new GregorianCalendar();
+		gcalendar.clear();
+		gcalendar.set(1901, 0, 1); // First day
+		gcalendar.add(Calendar.DAY_OF_MONTH, 5); // First Sunday
+
+		final GregorianCalendar lastDay = new GregorianCalendar();
+		lastDay.clear();
+		lastDay.set(2000, 11, 31); // Last day
+		
+		while (gcalendar.compareTo(lastDay) < 0) {
+			gcalendar.add(Calendar.DAY_OF_MONTH, 7);
+			if (gcalendar.get(Calendar.DAY_OF_MONTH) == 1) {
+				result++;
+			}
+		}
+
 		final double totalTime = (System.nanoTime() - startTime) / NANO_TO_S;
 		System.out.println("p019: " + result + " -> " + totalTime + " s");
 	}
