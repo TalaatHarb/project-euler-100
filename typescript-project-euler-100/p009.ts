bfd794af630457ec667249841c4ef77396d87c9d
mharb <mohamed.harb@cegedim.com>
2020-02-05 17:12:50 +0200

    Added solution for p009 in typescript

--- typescript-project-euler-100/p009.ts
+++ typescript-project-euler-100/p009.ts
@@ -1,11 +1,23 @@
 export class P009 {
     run() {
         const startTime = +new Date();
-        let result = 0;
+        const target_sum = 1000;
+		let product = 0;
+		for (let a = 1; a < (target_sum - 2); a++) {
+			for (let b = a + 1; b < (target_sum - 1); b++) {
+				const sumOfSquares = a * a + b * b;
+				let c = (Math.sqrt(sumOfSquares));
+				const sum = a + b + c;
+				if (sum == target_sum) {
+					c = Math.floor(c);
+					product = (a * b * c);
+				}
+			}
+		}
 
         const endTime = +new Date();
         const period = (endTime - startTime) / 1000.0;
-        console.log("p009: " + result + " -> " + period + " s");
+        console.log("p009: " + product + " -> " + period + " s");
     }
 }
 
