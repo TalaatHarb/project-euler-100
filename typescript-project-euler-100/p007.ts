3cbecc51560c45ddaa83966514f837170b799788
mharb <mohamed.harb@cegedim.com>
2020-02-05 16:09:32 +0200

    Added solution for p007 in typescript

--- typescript-project-euler-100/p007.ts
+++ typescript-project-euler-100/p007.ts
@@ -1,11 +1,39 @@
 export class P007 {
+
+    isPrime(number: number) {
+		if (number % 2 == 0) {
+			return false;
+		}
+		let upperLimit = Math.floor(Math.sqrt(number));
+		if (upperLimit % 2 == 0) {
+			upperLimit--;
+		}
+		for (let i = upperLimit; i > 1; i -= 2) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
     run() {
         const startTime = +new Date();
-        let result = 0;
+        const target = 10001;
+		let counter = 1;
+		let current = 0;
+
+		let i = 1;
+		while (counter <= target) {
+			if (this.isPrime(i)) {
+				current = i;
+				counter += 1;
+			}
+			i += 2;
+		}
 
         const endTime = +new Date();
         const period = (endTime - startTime) / 1000.0;
-        console.log("P007: " + result + " -> " + period + " s");
+        console.log("P007: " + current + " -> " + period + " s");
     }
 }
 
