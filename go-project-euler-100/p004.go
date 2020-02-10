3bfd2cb745a3a4831833d80bbca4cc77eefc4edf
mharb <mohamed.harb@cegedim.com>
2020-02-10 14:27:24 +0200

    Added solution for p004 in Go

--- go-project-euler-100/p004.go
+++ go-project-euler-100/p004.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
+
+func isPalindromic(number int) bool {
+	numberAsString := strconv.Itoa(number)
+	length := len(numberAsString)
+	result := true
+	j := 0
+	for i := 0; i < length/2; i++ {
+		j = length - i - 1
+		if numberAsString[i] != numberAsString[j] {
+			result = false
+			break
+		}
+	}
+	return result
+}
 
 /*P004Run Entry point*/
 func P004Run() {
 	const NS = 1.0e9
 	startTime := time.Now()
 	result := 0
+	const finish = 999
+	const start = 99
+	i := finish
 
+	for ; i > start; i-- {
+
+		for j := finish; j > start; j-- {
+
+			number := i * j
+			if (number > result) && isPalindromic(number) {
+				result = number
+				break
+			}
+		}
+	}
 	period := (time.Since(startTime).Nanoseconds()) / NS
 	fmt.Println("p004:", result, "->", period, "s")
 }
