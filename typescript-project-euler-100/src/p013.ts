8fa9806a1e6fb5cb573a2fd59d5905ec6cf23c8b
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-01-22 23:51:07 +0200

    solved p13 in typescript

--- typescript-project-euler-100/src/p013.ts
+++ typescript-project-euler-100/src/p013.ts
@@ -1,9 +1,22 @@
 import { Solution } from "./Solution";
+import { readFileSync } from "fs";
 
 export class P013 extends Solution{
     solve() {
         this.problemNumber = 13;
-        const result = 0;
-        return result;
+        let sum = BigInt(0);
+		const numbers = this.readNumbers("p013.txt");
+
+		for (const line of numbers) {
+			sum += BigInt(line);
+		}
+		const result = (sum.toString()).substring(0, 10);
+
+		return Number(result);
+    }
+
+    readNumbers(fileName: string): string[]{
+        const file = readFileSync(fileName, 'utf-8');
+        return file.split('\n');
     }
 }
--- typescript-project-euler-100/test/allSolutions.spec.ts
+++ typescript-project-euler-100/test/allSolutions.spec.ts
@@ -174,7 +174,7 @@ describe('All solutions', () => {
         expect(period).toBeLessThan(60);
     });
 
-    xit('should solve p013', () => {
+    it('should solve p013', () => {
         const problem: Solution = new P013();
         const startTime = +new Date();
 
