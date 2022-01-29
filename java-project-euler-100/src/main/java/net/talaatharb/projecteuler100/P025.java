19ffe7ffa5f9c6cb8bc13394aa8e6fa1e9a1701c
Mohamed Talaat Harb <mohamed.harb@cegedim.com>
2022-01-29 12:09:52 +0200

    solved p25 in java

--- java-project-euler-100/src/main/java/net/talaatharb/projecteuler100/P025.java
+++ java-project-euler-100/src/main/java/net/talaatharb/projecteuler100/P025.java
@@ -1,5 +1,9 @@
 package net.talaatharb.projecteuler100;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * P025
  * 
@@ -17,12 +21,39 @@ public class P025 extends Solution {
 		new P025().run();
 	}
 
+	private Map<Integer, BigInteger> memoryCache;
+
+	public P025() {
+		memoryCache = new HashMap<>();
+
+		memoryCache.put(1, BigInteger.valueOf(1));
+		memoryCache.put(2, BigInteger.valueOf(1));
+	}
+
+	BigInteger fib(final int n) {
+		if (memoryCache.containsKey(n)) {
+			return memoryCache.get(n);
+		}
+
+		final BigInteger result = fib(n - 2).add(fib(n - 1));
+		memoryCache.put(n, result);
+
+		return result;
+	}
+
 	@Override
 	public Number solve() {
 		problemNumber = 25;
-		final int result = 0;
+		final int n = 1000;
 
-		return result;
+		int i = 3;
+		BigInteger a = fib(i);
+		while (("" + a).length() < n) {
+			i++;
+			a = fib(i);
+		}
+
+		return i;
 	}
 
 }
