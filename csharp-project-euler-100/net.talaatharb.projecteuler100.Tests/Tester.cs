ef936be1e9e68ef4e1d1b2a26014c0171582e0e7
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2026-07-05 19:42:15 +0300

    Tester/SolveAndTest

---
+++ csharp-project-euler-100/net.talaatharb.projecteuler100.Tests/Tester.cs
@@ -0,0 +1,20 @@
+﻿using net.talaatharb.projecteuler100.Core;
+using System.Diagnostics;
+
+namespace net.talaatharb.projecteuler100.Tests;
+
+public class Tester
+{
+    public static void SolveAndTest(long expectedResult, ISolvable problem)
+    {
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        long result = problem.Solve();
+        stopwatch.Stop();
+        double period = stopwatch.Elapsed.TotalSeconds;
+
+        Console.WriteLine("p" + ("000" + problem.GetProblemNumber())[(problem.GetProblemNumber() + "").Length..] + ": " + result
+            + " -> " + period + " s");
+        Assert.Equal(expectedResult, result);
+    }
+}
